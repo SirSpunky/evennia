5a4093d513158f6aafabd8790658dd475bffcd7e
Sir Spunky <hannes.ryden@gmail.com>
2014-01-30 19:59:05 +0100

    Removed old code

--- game/gamesrc/scripts/__init__.py
+++ game/gamesrc/scripts/__init__.py
@@ -1,9 +1,1 @@
 # -*- coding: utf-8 -*-
-from repeatmessage import RepeatMessage
-from randommovement import RandomMovement
-from globaltime import GlobalTime
-from globalweather import GlobalWeather
-from globaldatabase import GlobalDatabase
-
-
-#__all__ = ("RepeatMessage")
